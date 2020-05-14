fdcdf6ce376f06ef3b063f4abe636f1831923fef
Fosfor Aaka <fosfor.software@seznam.cz>
2020-05-14 02:57:46 +0200

    added randomization functions

--- src/Functions_pkg.vhd
+++ src/Functions_pkg.vhd
@@ -13,6 +13,12 @@ package Functions_pkg is
 
   function clog2(x : natural) return positive;
 
+  type T_RAND is protected
+    impure function getReal(maxVal : real := 1.0; minVal : real := 0.0) return real;
+    impure function getInteger(maxVal : integer := 1; minVal : integer := 0) return integer;
+    impure function getTime(maxVal : time := 1 ns; minVal : time := 0 ns; unit : time := ns) return time;
+  end protected;
+
 end package;
 
 package body Functions_pkg is
@@ -24,5 +30,34 @@ package body Functions_pkg is
       return positive(ceil(log2(real(x))));
     end if;
   end function;
+
+  type T_RAND is protected body
+    
+    variable seed1, seed2 : integer := 1;
+
+    impure function getReal(maxVal : real := 1.0; minVal : real := 0.0) return real is
+      variable realValue: real;
+    begin
+      uniform(seed1, seed2, realValue);
+      return realValue * (maxVal - minVal) + minVal;
+    end function;
+    
+    impure function getInteger(maxVal : integer := 1; minVal : integer := 0) return integer is
+      variable realValue: real;
+    begin
+      uniform(seed1, seed2, realValue);
+      return integer(floor(realValue * real(maxVal - minVal + 1))) + minVal;
+    end function;
+    
+    impure function getTime(maxVal : time := 1 ns; minVal : time := 0 ns; unit : time := ns) return time is
+      variable intValue, maxValInt, minValInt: integer;
+    begin
+      maxValInt := maxVal / unit;
+      minValInt := minVal / unit;
+      intValue := getInteger(maxValInt, minValInt);
+      return intValue * unit;
+    end function;
+    
+  end protected body;
 
 end package body;
