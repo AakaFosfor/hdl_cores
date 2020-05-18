dc7d763b1c03185be109bab6c7b0cdc592f31d48
Fosfor Aaka <fosfor.software@seznam.cz>
2020-05-19 00:14:50 +0200

    added coding rules, Doxygen and an entity template

---
+++ doc/CodingRules.md
@@ -0,0 +1,15 @@
+# Coding Rules
+
+## Basic Text Rules
+
+1. Spaces, no TABs
+1. Two spaces
+
+## HDL
+
+### Signal and Port Naming
+
+| Prefix | Rule | Suffix |
+|--:|:--|:--:|
+| | Input port | \_i |
+| | Output port | \_o |
---
+++ src/_template.vhd
@@ -0,0 +1,31 @@
+-- XXX
+-- Aaka Fosfor, 2020
+-- https://github.com/AakaFosfor/hdl_cores
+--
+-- state: draft
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity XXX is
+  generic (
+    G_CLK_FREQUENCY : real; -- in Hz
+    XXX
+  );
+  port (
+    Clk_ik   : in  std_logic;
+    Reset_ir : in  std_logic;
+    XXX
+  );
+end entity;
+
+architecture RTL of XXX is
+
+  XXX
+
+begin
+
+  XXX
+
+end architecture;
